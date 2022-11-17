4227d2bd898696a668b87bacd1f007ec3b342f99
maheswaradevo <pundadevo21@gmail.com>
2022-11-17 22:04:31 +0800

    [#1]feat: add database migrations

---
+++ db/migrations/20221117133910_create_v1_ddl_tables.up.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY (id),
+    UNIQUE KEY unique_email(email)
+);
+
+CREATE TABLE IF NOT EXISTS categories(
+    id INT NOT NULL AUTO_INCREMENT,
+    type VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS tasks(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    status BOOLEAN,
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
