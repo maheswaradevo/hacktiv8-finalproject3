9f575cdabcc74cba55be843359d40849777948b2
Hanif Fadillah Amryudin <documenthanif16@gmail.com>
2022-11-24 21:06:40 +0700

    [#3] fix: change route status task

--- internal/task/handler/task_handler.go
+++ internal/task/handler/task_handler.go
@@ -29,7 +29,7 @@ func NewTaskHandler(r *gin.RouterGroup, ts task.TaskService) *gin.RouterGroup {
 	{
 		taskProtectedRoute.Handle(http.MethodPost, "/", delivery.createTask)
 		taskProtectedRoute.Handle(http.MethodGet, "/", delivery.viewTask)
-		taskProtectedRoute.Handle(http.MethodPatch, "/:taskId", delivery.updateTaskStatus)
+		taskProtectedRoute.Handle(http.MethodPatch, "update-status/:taskId", delivery.updateTaskStatus)
 		taskProtectedRoute.Handle(http.MethodDelete, "/:taskId", delivery.deleteTask)
 	}
 	return taskRoute
